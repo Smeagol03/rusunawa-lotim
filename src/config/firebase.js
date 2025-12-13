ab5614929474b8faa0f77f3e3fad016d97d4916d
Alpian Tabrani <atabrani3@gmail.com>
2025-12-13 15:13:36 +0800

    ganti env

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -2,15 +2,14 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getDatabase } from "firebase/database";
 const firebaseConfig = {
-  apiKey: "AIzaSyAOhjEc7CCKLLrEsHOAcPAorYHLwC667VQ",
-  authDomain: "rusunawalotim.firebaseapp.com",
-  databaseURL:
-    "https://rusunawalotim-default-rtdb.asia-southeast1.firebasedatabase.app",
-  projectId: "rusunawalotim",
-  storageBucket: "rusunawalotim.firebasestorage.app",
-  messagingSenderId: "992215679452",
-  appId: "1:992215679452:web:67e8976c48085dbd914f19",
-  measurementId: "G-81D2BKFF0G",
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  databaseURL: import.meta.env.VITE_FIREBASE_DATABASE_URL,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID,
 };
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
